c16df27db978edf201898f3a24e10f532e20791f
BEN OTHMAN KARIM p1910531 <karim.ben-othman@etu.univ-lyon1.fr>
2020-10-15 12:18:36 +0200

    V2 Stable : more option of control

--- ControlKa_Version_Pc.py
+++ ControlKa_Version_Pc.py
@@ -1,23 +1,50 @@
 from pynput.keyboard import Key, Controller
+from pynput.mouse import Button
+from pynput import mouse
+
 import time
 
 import socket
 
 clavier = Controller()
+souris = mouse.Controller()
 
-''' procedure inportante '''
+'''---- procedure inportante ----'''
+
 def tappe(touche):
     clavier.press(touche)
     clavier.release(touche)
+    
 def tappe_2(t1, t2):
     clavier.press(t1)
     clavier.press(t2)
     clavier.release(t2)
     clavier.release(t1)
-''' menu '''
+    
+def tappe_txt(txt):
+    for i in txt:
+        tappe(i)
+
+        
+'''---- fonctions generales ----'''
+
+def check_texte_non_codifier(code):
+    if len(code) != 0:
+        if code[0] != "#":
+            return True
+        else:
+            return False
+    else:
+        return False
+        
+'''---- menu ----'''
+
 def menu(code):
+    #tappe un texte
+    if check_texte_non_codifier(code):
+        tappe_txt(code)
     #espace pause play
-    if code == "#181902":
+    elif code == "#181902":
         tappe(Key.space)
     #plein ecran yt
     elif code == "#2582":
@@ -34,6 +61,22 @@ def menu(code):
     #gauche
     elif code == "#-100":
         tappe(Key.left)
+    #suppr
+    elif code == "#-300":
+        tappe(Key.backspace)
+    # 4 fleche
+    elif code == "#555":
+        souris.click(Button.right, 1)
+    elif code == "#-555":
+        souris.click(Button.left, 1)
+    elif code == "#999":
+        tappe(Key.up)
+    elif code == "#-999":
+        tappe(Key.down)
+    #entrer
+    elif code == "#222":
+        tappe(Key.enter)
+    #autre
     else:
         print("ce n'est pas une action prise en charge (ou arret d'ecoute)")
 
